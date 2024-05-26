fb2e6e7074e342e7a8dafe9fbf14d7e284b57dbf
yongj.zhuang <yongj.zhuang@outlook.com>
2024-05-26 12:05:17 +0800

    user-vault build

---
+++ chill/scripts/user-vault.sh
@@ -0,0 +1,15 @@
+build="user-vault_build"
+repo="user-vault"
+service="user-vault"
+
+cd ~/git/$repo \
+    && git fetch \
+    && git pull \
+    && CGO_ENABLED=0 GOOS="linux" GOARCH="amd64" /usr/local/go/bin/go build -o "$build" cmd/main.go \
+    && cp "$build" ~/services/$service/build/ \
+    && cp ./prod-conf.yml ~/services/$service/config/conf.yml \
+    && cp ./Dockerfile_local ~/services/$service/build/Dockerfile \
+    && cd ~/services \
+    && /usr/bin/docker-compose up -d --build $service \
+    && rm ~/git/$repo/$build
+
