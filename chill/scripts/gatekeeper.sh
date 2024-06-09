6144b4261acd0cde895b13d1866b5bb0004ef704
yongj.zhuang <yongj.zhuang@outlook.com>
2024-06-09 14:51:33 +0800

    Modified:  chill/conf_prod.yml Added:     chill/scripts/gatekeeper.sh

---
+++ chill/scripts/gatekeeper.sh
@@ -0,0 +1,15 @@
+build="gatekeeper_build"
+repo="gatekeeper"
+service="gatekeeper"
+
+cd ~/git/$repo \
+    && git fetch \
+    && git pull \
+    && CGO_ENABLED=0 GOOS="linux" GOARCH="amd64" /usr/local/go/bin/go build -o "$build" main.go \
+    && cp "$build" ~/services/$service/build/ \
+    && cp ./conf-prod.yml ~/services/$service/config/conf.yml \
+    && cp ./Dockerfile_local ~/services/$service/build/Dockerfile \
+    && cd ~/services \
+    && /usr/bin/docker-compose up -d --build $service \
+    && rm ~/git/$repo/$build
+
